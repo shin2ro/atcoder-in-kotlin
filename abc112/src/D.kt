0473efe6d0910595a146558f36ed8d89d90c1437
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-01-09 13:55:22 +0900

    :coffee: Solve AtCoder Beginner Contest 112
    - D. Partition

--- abc112/src/D.kt
+++ abc112/src/D.kt
@@ -1,5 +1,16 @@
 fun main(args: Array<String>) {
-    val (n, m) = readLine()!!.split(' ').map { it.toInt() }
-    val ans = (m / n downTo 1).first { (m - it * n) % it == 0 }
+    val (n, m) = readLine()!!.split(' ').map { it.toLong() }
+    var ans = 0L
+    var i = 1L
+    while (i * i <= m) {
+        if (m % i == 0L) {
+            for (d in arrayOf(i, m / i)) {
+                if (m >= n * d) {
+                    ans = Math.max(ans, d)
+                }
+            }
+        }
+        i++
+    }
     println(ans)
 }
