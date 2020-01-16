bccce5fc7323716f45697e66597ce6c9401d5e42
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-01-16 20:28:23 +0900

    :coffee: Solve AtCoder Beginner Contest 106
    - D. AtCoder Express 2

--- abc106/src/D.kt
+++ abc106/src/D.kt
@@ -5,14 +5,19 @@ fun main(args: Array<String>) {
     val t = Array(n + 1) { IntArray(n + 1) }
     repeat(m) {
         val (l, r) = readLine()!!.split(' ').map { it.toInt() }
-        for (i in 1..l)
-            for (j in r..n)
-                t[i][j]++
+        t[l][r]++
     }
+
+    for (i in 1..n) {
+        for (j in 1..n) {
+            t[i][j] += t[i - 1][j] + t[i][j - 1] - t[i - 1][j - 1]
+        }
+    }
+
     val pw = PrintWriter(System.out)
     repeat(q) {
         val (l, r) = readLine()!!.split(' ').map { it.toInt() }
-        pw.println(t[l][r])
+        pw.println(t[r][r] - t[l - 1][r] - t[r][l - 1] + t[l - 1][l - 1])
     }
     pw.flush()
 }
